b91e7be545f070df92f635853a2eae4268518908
Your Name <aisyahkhalidafajria@gmail.com>
2021-11-05 22:52:26 +0700

    up view_barang.php

--- view_barang.php
+++ view_barang.php
@@ -1,25 +1,31 @@
+
 <?php
 include "koneksi.php";
 ?>
-<table>
-<tr>
-    <td>No</td>
-    <td>Nama</td>
-    <td>Alamat</td>
-    <td>No Telpon</td>
-</tr>
 
-<?php
-    $no=1;
-    $query=mysqli_query($koneksi, "select * from barang");
-    while($list_barang=mysqli_fetch_array($query))
-    {
-    ?>
-    <tr>
-    <td><?php echo $no++?></td>
-    <td><?php echo $list_barang ['nama'];?></td>
-    <td><?php echo $list_barang ['alamat'];?></td>
-    <td><?php echo $list_barang ['no_telp'];?></td>
-    </tr>
-    <?php } ?>
+<a href ="input_barang.php" class="btn btn-danger">Tambah Barang</a>
+<table class="table table-dark">
+	<tr>
+		<td>No</td>
+		<td>Nama</td>
+		<td>Alamat</td>
+		<td>NO. Telpon</td>
+		<td colspan="4">Action</td>
+	</tr>
+	<?php
+		$no=1;
+		$query=mysqli_query($koneksi,"select * from barang");
+		while($list_barang=mysqli_fetch_array($query))
+		{
+		?>
+	<tr>
+		<td><?php echo $no++?></td>
+		<td><?php echo $list_barang ['nama'];?></td>
+		<td><?php echo $list_barang ['alamat'];?>
+		<td><?php echo $list_barang ['no_telp'];?>
+		<td><a href="edit_barang.php?id_barang=<?php echo $list_barang['id_barang'];?>">Edit</td>
+		<td><a href="hapus_barang.php?id_barang=<?php echo $list_barang['id_barang'];?>">Hapus</td>
+	</tr>	
+	<?php } ?>
+
 </table>
