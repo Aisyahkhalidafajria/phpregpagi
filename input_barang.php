d7e30a685639edcc769d6c29249b74532ce97af7
Your Name <aisyahkhalidafajria@gmail.com>
2021-11-05 22:57:56 +0700

    mrenambah file baru

--- input_barang.php
+++ input_barang.php
@@ -1,39 +1,38 @@
 <?php
-include("koneksi.php");
-
-if (isset($_POST['save'])){
-    $nama=$_POST['nama'];
-    $alamat=$_POST['alamat'];
-    $no_telp=$_POST['no_telp'];
-    $query=mysqli_query($koneksi,"INSERT INTO barang(nama, alamat, no_telp) 
-    value ('$nama','$alamat','$no_telp')");
+include "koneksi.php";
+if (isset($_POST['save'])) {
+		$nama=$_POST['nama'];
+		$alamat=$_POST['alamat'];
+		$no_telpn=$_POST['no_telp'];
+		$query=mysqli_query($koneksi,"insert into barang(nama,alamat, no_telp)
+		value ('$nama','$alamat','$no_telpn')");//tanda petik 1 & tanda petik 2 jangan sampai salah
 if($query) {
-    header ("location:view_barang.php");
-} else {
-    echo mysqli_eror ();
-
+	header ("location:view_barang.php"); //ini yang bakal tampil di database setelah diisi
+}	else {
+	echo mysqli_error ();
 }
 }
+		
 ?>
-<form method="POST">
-<table class="table table-bordered" border="0">
+
+<form method="POST" action="">
+<table class="table table-bordered" border="1">
 	<tr>
 		<td>Nama</td>
 		<td><input type="text" name="nama" class="form-control"></td>
 	</tr>
-    <tr>
-         <td>Alamat</td>
+	<tr>
+		<td>Alamat</td>
 		<td><input type="text" name="alamat" class="form-control"></td>
 	</tr>
-    <tr>
-	    <td>No Telepon</td>
+    <td>No Telepon</td>
 		<td><input type="text" name="no_telp" class="form-control"></td>
 	</tr>
 	
-	
 	</tr>
-		<tr>
+	<tr>
 			<td><input type="submit" name="save" class="btn btn-danger"></td>
-		</tr>
+	</tr>
 </table>
 </form>
+	
